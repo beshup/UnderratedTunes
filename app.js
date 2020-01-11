8106a1106efc7cc136d6f962f491c042b6f27b91
beshup <ishanghimire9@yahoo.ca>
2020-01-11 21:30:40 +0000

    Now storing data and calling data from DB

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
-var express = require('express');
-var app = express();
-var bodyParser= require('body-parser');
+var express    = require('express'),
+ 	app        = express(),
+ 	bodyParser = require('body-parser'),
+ 	mongoose   = require('mongoose')
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect('mongodb://localhost:27017/underratedTunes');
+
+//schema set up
+
+var tuneSchema = new mongoose.Schema({
+	name: String,
+	artist: String,
+	image: String
+});
+
+var Tune = mongoose.model("Tune", tuneSchema);
+/*
+Tune.create({
+	name: "Location", 
+	artist: "Dave", 
+	image: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+}, function(err, tune){
+	if(err){
+		console.log("error");
+		console.log(err);
+	}
+	else{
+		console.log("added tune to db");
+		console.log(tune);
+	}
+});
+*/
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
@@ -9,15 +43,16 @@ app.get('/', function(req, res){
 	res.render('landing');
 });
 
-var tunes = [
-		{name: "Location", artist: "Dave", image: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"},
-		{name: "Thiago Silva", artist: "Dave x AJ Tracey", image: "https://cdn.pixabay.com/photo/2012/03/01/00/21/bridge-19513__480.jpg"},
-		{name: "Fashion Week", artist: "Steel Banglez", image: "https://cdn.pixabay.com/photo/2013/07/18/20/26/boat-164989__480.jpg"}
-	];
-
 app.get('/tunes', function(req, res){
 	
-	res.render('tunes', {tunes: tunes});
+	Tune.find({}, function(err, tunes){
+		if (err){
+			console.log(err);
+		}
+		else{
+			res.render('tunes', {tunes: tunes});
+		}
+	});
 });
 
 app.post('/tunes', function(req, res){
@@ -27,8 +62,15 @@ app.post('/tunes', function(req, res){
 	var name = req.body.name;
 	var image = req.body.image;
 	var newTune = {name: name, image: image};
-	tunes.push(newTune);
-	res.redirect('/tunes');
+	// create new tunes and save to dB
+	Tune.create(newTune, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.redirect('/tunes');
+		}
+	});
 });
 
 app.get('/tunes/new', function(req, res){
