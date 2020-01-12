a4d04041c22b079522c74a420330d088951bd943
beshup <ishanghimire9@yahoo.ca>
2020-01-12 04:10:15 +0000

    Fully integrated DB, fully intgrated Spotify API

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express    = require('express'),
  	app        = express(),
  	bodyParser = require('body-parser'),
- 	mongoose   = require('mongoose')
+ 	mongoose   = require('mongoose'),
+	request    = require('request'),
+	fetch      = require('isomorphic-fetch'),
+	Spotify    = require('node-spotify-api')
+ 
+require('dotenv').config();
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
@@ -12,29 +17,23 @@ mongoose.connect('mongodb://localhost:27017/underratedTunes');
 
 //schema set up
 
+var spotify = new Spotify({
+  id: process.env.CLIENT_ID,
+  secret: process.env.CLIENT_SECRET
+});
+
 var tuneSchema = new mongoose.Schema({
-	name: String,
-	artist: String,
-	image: String
+	dbtrack: String,
+	dbtrackId: String,
+	dbimageurl: String,
+	dbartist: String,
+	dblinkToTrack: String,
+	dbpopularity: Number,
+	dbalbumName: String,
+	dbupvotes: Number
 });
 
 var Tune = mongoose.model("Tune", tuneSchema);
-/*
-Tune.create({
-	name: "Location", 
-	artist: "Dave", 
-	image: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
-}, function(err, tune){
-	if(err){
-		console.log("error");
-		console.log(err);
-	}
-	else{
-		console.log("added tune to db");
-		console.log(tune);
-	}
-});
-*/
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
@@ -43,6 +42,8 @@ app.get('/', function(req, res){
 	res.render('landing');
 });
 
+
+//INDEX - show all tunes
 app.get('/tunes', function(req, res){
 	
 	Tune.find({}, function(err, tunes){
@@ -50,33 +51,119 @@ app.get('/tunes', function(req, res){
 			console.log(err);
 		}
 		else{
-			res.render('tunes', {tunes: tunes});
+			res.render('index', {tunes: tunes});
 		}
 	});
 });
 
+
+//CREATE - add new tune to DB
 app.post('/tunes', function(req, res){
 	// get data from form 
 	// add to tunes array
 	// redirect back to tunes page
-	var name = req.body.name;
-	var image = req.body.image;
-	var newTune = {name: name, image: image};
-	// create new tunes and save to dB
-	Tune.create(newTune, function(err, newlyCreated){
-		if(err){
-			console.log(err);
+	var link = req.body.link;
+	var track = String,
+		trackID = String,
+		imageurl = String,
+		artist = String,
+		linkToTrack = String,
+		popularity = String,
+		albumName = String,
+		upvotes = 0;
+	
+	//console.log(link);
+	
+	var kcharPos=0;
+	for (var i=0; i<link.length; i++){
+		if(link.charAt(i)=='k'){
+			kcharPos = i;
+			break;
 		}
-		else{
-			res.redirect('/tunes');
+	}
+	kcharPos += 2;
+	
+	var questioncharPos=0;
+	
+	for (var i=kcharPos; i<link.length; i++){
+		if(link.charAt(i)=='?'){
+			questioncharPos = i;
+			break;
 		}
+	}
+	
+	trackID = link.slice(kcharPos, questioncharPos);
+	//console.log(trackID);
+	
+	//now gotta call spotify's API and get access token to access data
+	
+	spotify.request('https://api.spotify.com/v1/tracks/' + trackID)
+	.then(function(data) {
+    	//console.log(data); 
+		track = data['name'];
+		linkToTrack = data['external_urls']['spotify'];
+		popularity = data['popularity'];
+		artist = data['artists'][0]['name'];
+		if (data['artists'].length > 1){
+			for (var i=1; i<data['artists'].length; i++){
+				if (i==1){
+					artist += ' ft. ' + data['artists'][i]['name'];
+				}
+				else{
+					artist += ', ' + data['artists'][i]['name'];
+				}
+			}
+		}
+		albumName = data['album']['name'];
+		if (data['album']['images'].length > 0){
+			imageurl = data['album']['images'][0]['url'];
+		}
+		/*
+		console.log(track);
+		console.log(trackID);
+		console.log(albumName);
+		console.log(imageurl);
+		console.log(popularity);
+		console.log(artist);
+		console.log(linkToTrack);
+		*/
+		
+		var newTune = {dbtrack: track, dbtrackID: trackID, dbalbumName: albumName, dbimageurl: imageurl, 									dbpopularity: popularity, dbartist: artist, dblinkToTrack: linkToTrack};
+		// create new tunes and save to dB
+		
+		Tune.create(newTune, function(error, newlyCreated){
+			if(error){
+				console.log(error);
+			}
+			else{
+				res.redirect('/tunes');
+			}		
+    });
+	}).catch(function(err) {
+    	console.error('Error occurred: ' + err); 
 	});
 });
 
+
+//NEW - show form to create new tune
 app.get('/tunes/new', function(req, res){
 	res.render('new');
 });
 
+//SHOW - show more info about particular tune
+app.get('/tunes/:id', function(req, res){
+	//find tune with given id
+	Tune.findById(req.params.id, function(err, foundTune){
+		if(err){
+			console.log(err);
+		}
+		else{
+			//render show template with that tune
+			res.render('show', {tune: foundTune});
+		}
+	});
+});
+	
 app.listen(3000, function(){
 	console.log('Underrated Tunes server has started.');
 });
